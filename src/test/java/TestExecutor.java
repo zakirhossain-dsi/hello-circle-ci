1c9aec88e0fcd49d22720cb188630436c4e64357
Zakir <zakir@cefalo.com>
2021-01-13 12:10:56 +0600

    Added JUnit

--- src/test/java/TestExecutor.java
+++ src/test/java/TestExecutor.java
@@ -1,2 +1,10 @@
-public class TestExecutor{
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestExecutor extends TestCase {
+
+    @Test
+    public void testMe(){
+        assertEquals(0, 0);
+    }
 }
