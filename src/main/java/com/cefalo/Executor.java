0187aea7a432d78f403b75ac6abc51331d1a3b12
Zakir <zakir@cefalo.com>
2021-01-13 12:18:40 +0600

    Test update.

--- src/main/java/com/cefalo/Executor.java
+++ src/main/java/com/cefalo/Executor.java
@@ -3,5 +3,6 @@ package com.cefalo;
 public class Executor {
     public static void main(String[] args) {
         System.out.println("Hello world.");
+        System.out.println("Hello world.");
     }
 }
